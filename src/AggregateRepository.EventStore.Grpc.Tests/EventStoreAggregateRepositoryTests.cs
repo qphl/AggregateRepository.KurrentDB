f11e804d782f68fc97272b99a8a049821de62c81
Josh Pattie <josh.pattie@qphl.co>
2022-10-21 15:05:39 +0100

    SBT-9731 Updated EventStore project to .Net6 and added new .Net6 test project

---
+++ src/AggregateRepository.EventStore.Grpc.Tests/EventStoreAggregateRepositoryTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AggregateRepository.EventStore.Grpc.Tests
+{
+    internal class EventStoreAggregateRepositoryTests : AggregateRepositoryTestFixture
+    {
+        protected override void CleanUpRepository()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void InitRepository()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/AggregateRepository.EventStore.Grpc.Tests/TestEvent.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="TestEvent.cs" company="Cognisant">
+// Copyright (c) Cognisant. All rights reserved.
+// </copyright>
+
+namespace CorshamScience.AggregateRepository.EventStore.Grpc.Tests
+{
+    using System;
+
+    internal class TestEvent
+    {
+        public TestEvent(Guid eventId) => EventId = eventId;
+
+        public Guid EventId { get; }
+    }
+}
