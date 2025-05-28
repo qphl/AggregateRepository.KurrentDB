12819a71dd81e061f0c052b0a14b8e1f58e73096
Josh Pattie <josh.pattie@qphl.co>
2025-05-28 10:23:13 +0100

    Try to fail build

--- build.cmd
+++ build.cmd
@@ -10,9 +10,9 @@ SET TAG=%TAG:tags/=%
 dotnet restore .\src\AggregateRepository.Kurrent.sln -PackagesDirectory .\src\packages -Verbosity detailed
 
 dotnet format .\src\AggregateRepository.Kurrent.sln --severity warn --verify-no-changes -v diag
-if %errorlevel% neq 0 exit /b %errorlevel%
+if %errorlevel% neq 0 exit %errorlevel%
 
 dotnet test .\src\AggregateRepository.Kurrent.Tests\AggregateRepository.Kurrent.Tests.csproj
-if %errorlevel% neq 0 exit /b %errorlevel%
+if %errorlevel% neq 0 exit %errorlevel%
 
 dotnet pack .\src\AggregateRepository.Kurrent\AggregateRepository.Kurrent.csproj -o .\dist -p:Version="%VERSION%" -p:PackageVersion="%VERSION%" -p:Tag="%TAG%" -c Release
