3a96885bec61a7f1194efcd8afdb30bbed2ae543
Josh Pattie <josh.pattie@qphl.co>
2025-05-28 12:14:22 +0100

    Fixed missing rename in build script + removed unused script

--- build.cmd
+++ build.cmd
@@ -10,16 +10,16 @@ SET TAG=%TAG:tags/=%
 SET RUNTESTS=true
 IF NOT [%3]==[] (SET RUNTESTS=%3)
 
-dotnet restore .\src\AggregateRepository.Kurrent.sln -PackagesDirectory .\src\packages -Verbosity detailed
+dotnet restore .\src\AggregateRepository.KurrentDB.sln -PackagesDirectory .\src\packages -Verbosity detailed
 
-dotnet format .\src\AggregateRepository.Kurrent.sln --severity warn --verify-no-changes -v diag
+dotnet format .\src\AggregateRepository.KurrentDB.sln --severity warn --verify-no-changes -v diag
 IF %errorlevel% neq 0 EXIT /B %errorlevel%
 
 IF /I "%RUNTESTS%"=="true" (
-	dotnet test .\src\AggregateRepository.Kurrent.Tests\AggregateRepository.Kurrent.Tests.csproj
+	dotnet test .\src\AggregateRepository.KurrentDB.Tests\AggregateRepository.KurrentDB.Tests.csproj
 	IF %errorlevel% neq 0 EXIT /B %errorlevel%
 ) ELSE (
 	ECHO Skipping tests because RUNTESTS is not set to "true".
 )
 
-dotnet pack .\src\AggregateRepository.Kurrent\AggregateRepository.Kurrent.csproj -o .\dist -p:Version="%VERSION%" -p:PackageVersion="%VERSION%" -p:Tag="%TAG%" -c Release
+dotnet pack .\src\AggregateRepository.KurrentDB\AggregateRepository.KurrentDB.csproj -o .\dist -p:Version="%VERSION%" -p:PackageVersion="%VERSION%" -p:Tag="%TAG%" -c Release
